7d5ccfe67c9f51a197d2ebd2f425d473de66b24b
6710110151 <6710110151@psu.ac.th>
2025-02-18 10:36:52 +0700

    add pic and fix jumping bug

--- Geometry.py
+++ Geometry.py
@@ -4,16 +4,20 @@ from kivy.uix.image import Image
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.clock import Clock
 from kivy.core.window import Window
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, BooleanProperty
+
 
 class Player(Image):
     velocity = NumericProperty(0)
     gravity = -0.5
     jump_strength = 10
+    on_ground = BooleanProperty(True)  # เพิ่มตัวแปรเพื่อตรวจสอบว่าตัวละครอยู่บนพื้นหรือไม่
     
     def jump(self):
-        self.velocity = self.jump_strength
-    
+        if self.on_ground:  # กระโดดได้เฉพาะเมื่ออยู่บนพื้น
+            self.velocity = self.jump_strength
+            self.on_ground = False  # เมื่อลอยขึ้น ให้ถือว่าไม่อยู่บนพื้น
+
     def update(self):
         self.velocity += self.gravity
         self.y += self.velocity
@@ -21,11 +25,12 @@ class Player(Image):
         if self.y <= 0:
             self.y = 0
             self.velocity = 0
+            self.on_ground = True  # ตั้งค่าเป็น True เมื่อแตะพื้น
 
 class GameScreen(RelativeLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.player = Player(source='cube.png', size_hint=(None, None), size=(50, 50), pos=(100, 100))
+        self.player = Player(source='cube_85.png', size_hint=(None, None), size=(50, 50), pos=(100, 100))
         self.add_widget(self.player)
         Clock.schedule_interval(self.update, 1/60)
         Window.bind(on_key_down=self.on_key_down)
