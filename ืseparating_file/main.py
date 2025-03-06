c47ae427b36c163a673a4040e442bcd1697dafff
6710110151 <6710110151@psu.ac.th>
2025-03-06 22:03:53 +0700

    separate main from main

---
+++ ืseparating_file/main.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager
+from home_screen import HomePage
+from stage_selection_screen import StageSelectionScreen
+from game_screen import GameScreen
+
+class MyApp(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(HomePage(name='home'))
+        sm.add_widget(StageSelectionScreen(name='stage_selection'))
+        sm.add_widget(GameScreen(name='game'))
+        return sm
+
+if __name__ == '__main__':
+    MyApp().run()
