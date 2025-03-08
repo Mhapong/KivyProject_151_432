c44041c20178ebee08307713c6e93ad3d88c058d
SaktanuthPeak <thanakrit.5122548@gmail.com>
2025-03-08 10:59:41 +0700

    fix skin selector

--- game_screen.py
+++ game_screen.py
@@ -15,7 +15,7 @@ import json
 
 class GameScreen(Screen):
     # Add this property
-    player_skin = StringProperty('assets/images/cube_5.png')  # Default skin
+    player_skin = StringProperty('assets/image/cube_5.png')  # Default skin
     
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
